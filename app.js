cc01c3811950a7d0c8309475fac4df1dce2c4e06
Gergely Lekli <glekli@users.noreply.github.com>
2016-03-10 18:32:18 -0800

    Fix version number in health check response. (#16)

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
 
 var server = require('./lib/server');
 var configManager = require('./lib/config-manager');
+var packageInfo = require('./package.json');
 var configFile = process.argv[2] ? process.argv[2] : process.cwd() + '/nodejs.config.js';
+
+global.version = packageInfo.version;
 
 configManager.readFromDisk(configFile);
 server.start(configManager);
